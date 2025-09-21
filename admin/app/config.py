39d2f105a724dca9ca415c2ecf526838ba0a2263
Jones <ndzenyuyjones@gmail.com>
2025-09-22 00:50:21 +0100

    added logging for ecs to pick

--- admin/app/config.py
+++ admin/app/config.py
@@ -5,9 +5,9 @@ load_dotenv()
 
 class Settings:
     AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_ACCOUNT_ID = os.getenv("AWS_ACCOUNT_ID")
+    # AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    # AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    # AWS_ACCOUNT_ID = os.getenv("AWS_ACCOUNT_ID")
     APP_CONFIG_TABLE = os.getenv("APP_CONFIG_TABLE", "Applications")
 
 settings = Settings()
--- requestor/app/main.py
+++ requestor/app/main.py
@@ -4,8 +4,15 @@ from fastapi import FastAPI, HTTPException
 from .models import NotificationRequest
 from .sqs_client import send_message_to_queue
 import logging
+import sys
 
 app = FastAPI()
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s [%(name)s] %(message)s"
+)
 
 @app.get("/health")
 def health_check():
@@ -16,7 +23,7 @@ def notify(req: NotificationRequest):
     try:
         response = send_message_to_queue(req.dict())
         logging.info(f"NotificationRequest payload: {response}")
-        print(f"NotificationRequest payload: {req.dict()}")
+        logging.info(f"NotificationRequest payload: {req.dict()}")
         return {
             "message_id": response.get("MessageId"),
             "status": "queued"
