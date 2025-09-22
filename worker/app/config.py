4a9429ee94147b1d7981018a6778a3a3f48af018
Jones <ndzenyuyjones@gmail.com>
2025-09-22 23:43:21 +0100

    updated the value of sqs

--- worker/app/config.py
+++ worker/app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AWS_REGION = os.getenv("AWS_REGION")
-SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL", "http://localstack:4566/000000000000/notifications-queue")
+SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL", "https://sqs.us-east-1.amazonaws.com/975391191374/YANTECH-YNP01-AWS-sqs-notification-requests-dev")
 APPLICATIONS_TABLE = os.getenv("APPLICATIONS_TABLE")
 REQUEST_LOG_TABLE = os.getenv("REQUEST_LOG_TABLE")
 
