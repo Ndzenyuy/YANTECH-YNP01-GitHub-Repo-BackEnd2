2986806507c95d9ac5c4b802d363fd240ecd4f22
Jones <ndzenyuyjones@gmail.com>
2025-09-25 13:32:15 +0100

    added account id

--- admin/app/config.py
+++ admin/app/config.py
@@ -7,7 +7,7 @@ class Settings:
     AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
     # AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     # AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    # AWS_ACCOUNT_ID = os.getenv("AWS_ACCOUNT_ID")
+    AWS_ACCOUNT_ID = os.getenv("AWS_ACCOUNT_ID", "975391191374")
     APP_CONFIG_TABLE = os.getenv("APP_CONFIG_TABLE", "YANTECH-YNP01-AWS-appTable-dev")
 
 settings = Settings()
